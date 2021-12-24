8b6542f5c22c3120be262e9a78510f987c436de0
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-12-24 19:16:07 +0700

    add code up 9 video

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
 const NavBar = () => {
+    const { currentUser } = useAuth();
+    console.log(currentUser);
+
     return (
-        <ul className="nav">
-            <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
-                    Main
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/login">
-                    Login
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/users">
-                    Users
-                </Link>
-            </li>
-        </ul>
+        <nav className="navbar bg-light mb-3">
+            <div className="container-fluid">
+                <ul className="nav">
+                    <li className="nav-item">
+                        <Link className="nav-link " aria-current="page" to="/">
+                            Main
+                        </Link>
+                    </li>
+                    {currentUser && (
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link"
+                                aria-current="page"
+                                to="/users"
+                            >
+                                Users
+                            </Link>
+                        </li>
+                    )}
+                    <li className="nav-item"></li>
+                </ul>
+                <div className="d-flex">
+                    {currentUser ? (
+                        <div className="nav-link">{currentUser.name}</div>
+                    ) : (
+                        <Link
+                            className="nav-link "
+                            aria-current="page"
+                            to="/login"
+                        >
+                            Login
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </nav>
     );
 };
 
