3d7600fb29f73100b6787debd39e746749e24428
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-12-25 18:22:30 +0700

    add code up 18-1 video

--- src/app/components/page/usersListPage/usersListPage.jsx
+++ src/app/components/page/usersListPage/usersListPage.jsx
@@ -8,9 +8,11 @@ import UserTable from "../../ui/usersTable";
 import _ from "lodash";
 import { useUser } from "../../../hooks/useUsers";
 import { useProfessions } from "../../../hooks/useProfession";
+import { useAuth } from "../../../hooks/useAuth";
 
 const UsersListPage = () => {
     const { users } = useUser();
+    const { currentUser } = useAuth();
     const { isLoading: professionsLoading, professions } = useProfessions();
     const [currentPage, setCurrentPage] = useState(1);
     const [searchQuery, setSearchQuery] = useState("");
@@ -53,20 +55,25 @@ const UsersListPage = () => {
         setSortBy(item);
     };
 
-    const filteredUsers = searchQuery
-        ? users.filter(
-              (user) =>
-                  user.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !==
-                  -1
-          )
-        : selectedProf
-        ? users.filter(
-              (user) =>
-                  JSON.stringify(user.profession) ===
-                  JSON.stringify(selectedProf)
-          )
-        : users;
+    function filterUsers(data) {
+        const filteredUsers = searchQuery
+            ? data.filter(
+                  (user) =>
+                      user.name
+                          .toLowerCase()
+                          .indexOf(searchQuery.toLowerCase()) !== -1
+              )
+            : selectedProf
+            ? data.filter(
+                  (user) =>
+                      JSON.stringify(user.profession) ===
+                      JSON.stringify(selectedProf)
+              )
+            : data;
+        return filteredUsers.filter((item) => item._id !== currentUser._id);
+    }
 
+    const filteredUsers = filterUsers(users);
     const count = filteredUsers.length;
     const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
     const usersCrop = paginate(sortedUsers, currentPage, pageSize);
