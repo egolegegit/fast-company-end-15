af3ddfa6f304f4d1fffc6fd2378e95d771be5c40
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-12-25 16:25:37 +0700

    add code up 17 video

--- src/app/hooks/useUsers.jsx
+++ src/app/hooks/useUsers.jsx
@@ -31,13 +31,18 @@ const UserProvider = ({ children }) => {
             setError(null);
         }
     }, [error]);
+
+    function getUserById(userId) {
+        return users.find((item) => item._id === userId);
+    }
+
     function errorCatcher(error) {
         const { message } = error.response.data;
 
         setError(message);
     }
     return (
-        <UserContext.Provider value={{ users }}>
+        <UserContext.Provider value={{ users, getUserById }}>
             {!isLoading ? children : "Loading...."}
         </UserContext.Provider>
     );
